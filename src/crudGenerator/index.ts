4a6ba5990fcba61d4e5f6e04e2bc9b574acca1ba
John Mendez <john.manuel.mendez@gmail.com>
2022-11-02 01:15:35 -0400

    Filter out models

--- src/crudGenerator/index.ts
+++ src/crudGenerator/index.ts
@@ -1,47 +1,68 @@
-import path from 'node:path';
-import { DMMF } from '@prisma/generator-helper';
-import { ConfigInternal } from '../utils/config';
-import { writeFile } from '../utils/filesystem';
-import { useTemplate } from '../utils/template';
-import { utilsTemplate, objectsTemplate, autoCrudTemplate } from './templates/root';
-import { generateModel } from './utils/generator';
+import path from "node:path";
+import { DMMF } from "@prisma/generator-helper";
+import { ConfigInternal } from "../utils/config";
+import { writeFile } from "../utils/filesystem";
+import { useTemplate } from "../utils/template";
+import {
+  utilsTemplate,
+  objectsTemplate,
+  autoCrudTemplate,
+} from "./templates/root";
+import { generateModel } from "./utils/generator";
 
-export async function generateCrud(config: ConfigInternal, dmmf: DMMF.Document): Promise<void> {
+export async function generateCrud(
+  config: ConfigInternal,
+  dmmf: DMMF.Document,
+): Promise<void> {
   if (config.crud.disabled) return;
 
-  const modelNames = dmmf.datamodel.models.map((model) => model.name);
+  const exposedModels = new Set(config.crud.exposedModels ?? []);
+
+  const modelNames = dmmf.datamodel.models
+    .map((model) => model.name)
+    .filter((modelName) => {
+      return !exposedModels.length || exposedModels.has(modelName);
+    });
 
   // Generate CRUD directories (e.g. User, Comment, ...)
-  await Promise.all(modelNames.map((model) => generateModel(config, dmmf, model)));
+  await Promise.all(
+    modelNames.map((model) => generateModel(config, dmmf, model)),
+  );
 
   // Generate root objects.ts file (export all models + prisma objects)
   const exports = dmmf.datamodel.models
     .map((model) => `export * from './${model.name}';`)
-    .join('\n');
-  const modelNamesEachLine = modelNames.map((model) => `'${model}',`).join('\n  ');
+    .join("\n");
+  const modelNamesEachLine = modelNames
+    .map((model) => `'${model}',`)
+    .join("\n  ");
 
   await writeFile(
     config,
-    'crud.objects',
-    useTemplate(objectsTemplate, { exports, ...config.crud, modelNames: modelNamesEachLine }),
-    path.join(config.crud.outputDir, 'objects.ts'),
+    "crud.objects",
+    useTemplate(objectsTemplate, {
+      exports,
+      ...config.crud,
+      modelNames: modelNamesEachLine,
+    }),
+    path.join(config.crud.outputDir, "objects.ts"),
   );
 
   // Generate root utils.ts file
   await writeFile(
     config,
-    'crud.utils',
+    "crud.utils",
     useTemplate(utilsTemplate, config.crud),
-    path.join(config.crud.outputDir, 'utils.ts'),
+    path.join(config.crud.outputDir, "utils.ts"),
   );
 
   // Generate root autocrud.ts file
   if (config.crud.generateAutocrud) {
     await writeFile(
       config,
-      'crud.autocrud',
+      "crud.autocrud",
       useTemplate(autoCrudTemplate, config.crud),
-      path.join(config.crud.outputDir, 'autocrud.ts'),
+      path.join(config.crud.outputDir, "autocrud.ts"),
     );
   }
 }
